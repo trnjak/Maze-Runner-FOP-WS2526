0a7b519da0b786748468c6bd684b09708d6de92c
Marko Trnjak <trrnjak@gmail.com>
2026-01-19 19:09:04 +0100

    Implemented line of sight check for enemy and added Javadoc documentation for the ChaserEnemy and PatrolEnemy.

--- core/src/de/tum/cit/fop/maze/objects/enemies/ChaserEnemy.java
+++ core/src/de/tum/cit/fop/maze/objects/enemies/ChaserEnemy.java
@@ -5,59 +5,57 @@ import com.badlogic.gdx.math.Vector2;
 import de.tum.cit.fop.maze.GameMap;
 import de.tum.cit.fop.maze.objects.Player;
 
-
-
-
+/**
+ * The ChaserEnemy class represents an enemy that chases the player when within line of sight and range.
+ * If not within said line of sight and range, the enemy wanders randomly.
+ */
 public class ChaserEnemy extends Enemy {
-    private float dx = 0, dy = 0, cd = 0; //direction to x, y, and change of direction respectively
+    private float dx = 0, dy = 0, cd = 0;
 
+    /**
+     * Creates a new ChaserEnemy at the specified position.
+     *
+     * @param x The x-coordinate of the enemy's starting position
+     * @param y The y-coordinate of the enemy's starting position
+     */
     public ChaserEnemy(int x, int y) {
         super(x, y, 10, 1);
     }
 
+    /**
+     * Updates the enemy's movement based on the player's position and game state.
+     * The enemy will chase the player when within line of sight and range,
+     * using obstacle avoidance to navigate around walls. When not chasing,
+     * the enemy moves randomly. If the enemy reaches the player, it pushes
+     * the player away and stops moving.
+     * @param delta The time elapsed since the last frame update
+     * @param player The player character being chased
+     * @param map The game map containing wall collision data
+     */
     @Override
     protected void move(float delta, Player player, GameMap map) {
-        if (inRange(player)) { //
-            // stop to attack player
+        if (inRange(player)) {
             dx = 0;
             dy = 0;
             return;
-
-//            Vector2 away = new Vector2(x - player.getX(), y - player.getY());
-//            if (away.len2() > 0) away.nor();
-//
-//            dx = away.x * 0.2f; // slow retreat
-//            dy = away.y * 0.2f;
         }
-        //TODO: implement chasing player with a*
-        if (inChaseRange(player)) {
-//            Vector2 dir = new Vector2(player.getX() - x, player.getY() - y); //move towards player
-//            if(dir.len2() > 0) {
-//                dir.nor();
-//                dx = dir.x;
-//                dy = dir.y;
-//            }
+        float ogX = x;
+        float ogY = y;
+        if (inChaseRange(player) && hasLineOfSight(player, map)) {
             Vector2 dir = new Vector2(player.getX() - x, player.getY() - y);
             if (dir.len2() > 0) {
                 dir.nor();
             }
             float lookDistance = speed * delta;
-            // this specifies how far ahead should i check
             float nextX = x + dir.x * lookDistance;
             float nextY = y + dir.y * lookDistance;
 
             Rectangle nextRec = new Rectangle(nextX, nextY, w, h);
-            // i added two instances which are the w and the h to the enemy class
-//            this is where enemy is going to be after moving or after
-//            trying to catch up on the player
             if (map.collidesWithWall(nextRec)) {
-                Vector2 tryDir = new Vector2(dir);//we are going ro use the old direction and change it a bit
-//                boolean path=false;
                 Vector2 bestDir = new Vector2(dir);
                 boolean found = false;
 
                 for (int angle = 15; angle <= 90; angle += 15) {
-                    // try right
                     Vector2 right = new Vector2(dir).rotateDeg(angle);
                     nextX = x + right.x * lookDistance;
                     nextY = y + right.y * lookDistance;
@@ -68,7 +66,6 @@ public class ChaserEnemy extends Enemy {
                         break;
                     }
 
-                    // try left
                     Vector2 left = new Vector2(dir).rotateDeg(-angle);
                     nextX = x + left.x * lookDistance;
                     nextY = y + left.y * lookDistance;
@@ -83,37 +80,21 @@ public class ChaserEnemy extends Enemy {
                 if (found) {
                     dir.set(bestDir);
                 } else {
-                    dir.setZero(); // stuck in a corner
+                    dir.setZero();
                 }
-
-//                if(!path){
-//                    dir.set(0,0);
-//                    rarely happens in case it blocked in a corner it can
-//                    not happen because it will already try to move up to 120 degrees
-//                }
-
-
             }
             dx = dir.x;
             dy = dir.y;
-//            In this lines i m implelmetnit the moves and now i m trying to od the d
-
-            float sx = dx * speed * delta;
-            float sy = dy * speed * delta;
-
-
 
             if (dir.len2() > 0) {
                 lookX = dir.x;
                 lookY = dir.y;
             }
 
-
         } else {
-            // wander randomly if player is outside of range
             cd += delta;
             if (cd > 2) {
-                dx = (int) (Math.random() * 3) - 1; //0,1,2 w/0 the -1, -1,0,1 with, so it's left/stay/right
+                dx = (int) (Math.random() * 3) - 1;
                 dy = (int) (Math.random() * 3) - 1;
                 Vector2 dir = new Vector2(dx, dy);
                 if (dir.len2() > 0) {
@@ -124,35 +105,29 @@ public class ChaserEnemy extends Enemy {
                 cd = 0;
             }
         }
-        Rectangle pb = player.getBounds();
-        float sx = dx * speed * delta; //step toward x
-        float sy = dy * speed * delta; //step toward y
 
+        float sx = dx * speed * delta;
+        float sy = dy * speed * delta;
 
         Rectangle next = new Rectangle(x + sx, y + sy, w, h);
-        // move if no collision, otherwise attempt axis aligned movement
+
         if (!map.collidesWithWall(next)) {
             x += sx;
             y += sy;
-//            if (next.overlaps(pb)){
-//                player.damage();
-//            }
-
-
         } else {
-            Rectangle tryX = new Rectangle(x + sx, y, w, h); //bounds for next x movement
-            Rectangle tryY = new Rectangle(x, y + sy, w, h); //same for y
-
+            Rectangle tryX = new Rectangle(x + sx, y, w, h);
             if (!map.collidesWithWall(tryX)) {
-                x += sx; //move along x if possible
-//                the  overlop exists and the enemy can not move anymore
+                x += sx;
             }
+            Rectangle tryY = new Rectangle(x, y + sy, w, h);
             if (!map.collidesWithWall(tryY)) {
-                y += sy; //move along y if possible
+                y += sy;
             }
-//            if (next.overlaps(pb)) {
-//                player.damage();  // or whatever your damage logic is
-//            }
+            Rectangle stuck = new Rectangle(x, y, w, h);
+            if (map.collidesWithWall(stuck)) {
+                x = ogX;
+                y = ogY;
+            }
         }
 
         if (dx != 0 || dy != 0) {
@@ -160,18 +135,54 @@ public class ChaserEnemy extends Enemy {
             lookX = dx / len;
             lookY = dy / len;
         }
+
         Rectangle enemyBounds = new Rectangle(x, y, w, h);
         Rectangle playerBounds = player.getBounds();
 
         if (enemyBounds.overlaps(playerBounds)) {
-
             pushPlayer(player);
         }
     }
 
     private boolean inChaseRange(Player player) {
-        return distance(player) <= 128; //chase range 128 pixels
+        return distance(player) <= 192;
     }
+
+    /**
+     * Checks whether the enemy has a line of sight to the player or not,
+     * used to ensure that the enemy won't try to chase a player from behind a wall.
+     * @param player The player chased by the enemy.
+     * @param map The game map.
+     * @return True if the enemy can see the player, false otherwise.
+     */
+    private boolean hasLineOfSight(Player player, GameMap map) {
+        Vector2 enemyPos = new Vector2(x + w / 2, y + h / 2);
+        Vector2 playerPos = new Vector2(player.getX() + (float) TILE / 2, player.getY() + (float) TILE / 2);
+
+        Vector2 dir = new Vector2(playerPos).sub(enemyPos);
+        float len = dir.len();
+        dir.nor();
+        float stepSize = 4;
+        int steps = (int) (len / stepSize);
+
+        Vector2 cur = new Vector2(enemyPos);
+
+        for(int i = 0; i < steps; i++) {
+            cur.add(dir.x * stepSize, dir.y * stepSize);
+            Rectangle checkRect = new Rectangle(cur.x - 2, cur.y - 2, 4, 4);
+            if(map.collidesWithWall(checkRect)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Pushes the player away from the enemy when they collide.
+     * The push direction is calculated as the vector from the enemy to the player,
+     * normalised and scaled by a fixed strength.
+     * @param player The player to push.
+     */
     protected void pushPlayer(Player player) {
         Vector2 push = new Vector2(
                 player.getX() - x,
@@ -179,7 +190,7 @@ public class ChaserEnemy extends Enemy {
         );
 
         if (push.len2() > 0) {
-            push.nor().scl(10f); // push strength (pixels)
+            push.nor().scl(10f);
             player.setPosition(
                     player.getX() + push.x,
                     player.getY() + push.y
--- core/src/de/tum/cit/fop/maze/objects/enemies/PatrolEnemy.java
+++ core/src/de/tum/cit/fop/maze/objects/enemies/PatrolEnemy.java
@@ -4,35 +4,54 @@ import com.badlogic.gdx.math.Rectangle;
 import de.tum.cit.fop.maze.GameMap;
 import de.tum.cit.fop.maze.objects.Player;
 
+/**
+ * The PatrolEnemy class represents an enemy that patrols along a fixed axis (either horizontal or vertical).
+ */
 public class PatrolEnemy extends Enemy {
-    private final boolean axis; //where's it headed? x-axis or y-axis
-    private float dir = 1; //direction of movement 1 for going right/up, -1 for going left/down
+    private final boolean axis;
+    private float dir = 1;
 
+    /**
+     * Creates a new PatrolEnemy at the specified position with the given patrol axis.
+     *
+     * @param x The x-coordinate of the enemy's starting position
+     * @param y The y-coordinate of the enemy's starting position
+     * @param axis The axis along which the enemy patrols (true for x-axis/horizontal, false for y-axis/vertical)
+     */
     public PatrolEnemy(int x, int y, boolean axis) {
         super(x, y, 10, 2);
         this.axis = axis;
         this.speed = 150;
     }
 
+    /**
+     * Updates the enemy's movement based on its patrol pattern.
+     * The enemy moves along its designated axis at a constant speed and
+     * reverses direction when it encounters walls or collides with the player.
+     * If the player enters attack range, the enemy stops moving to attack.
+     * @param delta The time elapsed since the last frame update
+     * @param player The player character
+     * @param map The game map
+     */
     @Override
     protected void move(float delta, Player player, GameMap map) {
-        float dx = 0, dy = 0; //direction towards x and y respectively
-        if(inRange(player)) { // stop to attack player
+        float dx = 0, dy = 0;
+        if(inRange(player)) {
             return;
         }
         if(axis) {
-            dx = dir * speed * delta; //if it's true (x-axis) move along x
+            dx = dir * speed * delta;
         } else {
-            dy = dir * speed * delta; //same for y
+            dy = dir * speed * delta;
         }
 
         Rectangle pb = player.getBounds();
         Rectangle next = new Rectangle(x + dx, y + dy, w, h);
 
-        if(!map.collidesWithWall(next) && !next.overlaps(pb)) { //move if it doesn't collide
+        if(!map.collidesWithWall(next) && !next.overlaps(pb)) {
             x += dx;
             y += dy;
-        } else { //change direction
+        } else {
             dir *= -1f;
         }
 
